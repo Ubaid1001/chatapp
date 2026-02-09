124af909e02a41d91e6131d22c0a7c6472227764
Mustafa <mustafaraza6372@gmail.com>
2026-02-09 16:37:22 +0500

    mera kaam

---
+++ Backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoute from './routes/message.route.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+console.log(process.env.PORT);
+
+app.use("/api/auth", authRoutes);
+app.use('/api/messages', messageRoute);
+
+
+app.listen(PORT, () => console.log("Server running on port: " + PORT)); 
