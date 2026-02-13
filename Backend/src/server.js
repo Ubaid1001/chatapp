102fc2211d5f2f129d92809e07c35735a19cb51d
Mustafa <mustafaraza6372@gmail.com>
2026-02-13 19:22:30 +0500

    register kam

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,17 +3,22 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoute from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000
 
 const app = express();
-
 console.log(process.env.PORT);
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use('/api/messages', messageRoute);
 
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT)); 
+app.listen(PORT, () => {
+    console.log("Server running on port: " + PORT),
+    connectDB()
+}); 
