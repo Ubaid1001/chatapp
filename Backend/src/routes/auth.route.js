75ca4e12694919a654558e7d19ba02c20f0f0f36
Ubaid1001 <ubaid1001@gmail.com>
2026-02-26 15:05:01 +0500

    arject lagadee

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { signup, signin, logout, updateProfile, } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js"
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 
 const router = express.Router();
@@ -10,5 +11,9 @@ router.post("/signin", signin);
 router.post("/logout", logout);
 router.put("/updateProfile", protectRoute, updateProfile);
 router.get('/check', protectRoute, (req, res) => res.status(200).json(req.user));
+
+router.get("/test",arcjetProtection,(req,res)=>{
+    res.status(200).json({message:"test route"})
+});
 
 export default router
